5b338f318fff1616680236cac12c0bd9548b4cd5
kancharanadi <kaditya090205@gmail.com>
2024-10-27 03:19:39 -0700

    Create prog1.c

---
+++ prog1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,n3,n4,n5;
+int sum,avh;
+printf("nos=");
+scanf("%d %d %d %d %d",&n1,&n2,&n3,&n4,&n5);
+sum=n1+n2+n3+n4+n5;
+avh=sum/5;
+printf("nosum=%d",sum);
+printf("noavg=%d",avh);
+retun 0;
+}
+
